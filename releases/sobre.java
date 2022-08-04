f2bcb55a5b749ff6ae43812bcecf3d0f5b58915f
Beatriz Nataly <84404795+BeatrizNataly@users.noreply.github.com>
2022-08-04 16:07:01 -0300

    Delete sobre.java

--- releases/sobre.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("E - T I C K E T S\n Projeto por: Beatriz Nataly\nDisponível em: https://github.com/BeatrizNataly/e-Tickets\n\n");
-        System.out.println("Bem-vindo(a) ao E-tickets.\nNesta pasta de releases você encontrará as pré versões fechadas do aplicativo para testar!");
-        System.out.println("Leve em consideração que o maior ponto de enfoque deste projeto é a interface, mas a aba de issues continuará aberta para sugestões de melhoria de aviso de bugs\n");
-        System.out.println("Escolha o zip que deseja executar, extraia e abra ele com Android Studio. Não é necessário incluir este arquivo .java ao projeto.");
-        System.out.println("Divirta-se!");
-    }
-}
